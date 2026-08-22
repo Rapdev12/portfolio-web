466c118e6cea3e59fc740add589617a23a01376e
Rapdev12 <rpalacioso12@outlook.com>
2026-08-22 20:43:21 -0300

    Arreglo de errores

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,19 +1,22 @@
+import logoPortfolio from "../../public/laptop-with-glowing-screen-table-dark-top-view-copy-space.jpg"
+import logoPelisweb from "../../public/Screenshot 2026-08-22 at 20-33-51 pelisweb.png"
+
 export const projects = [
   {
-    title: "Project One",
-    image: "/project-one.png",
-    description: "Descripción resumida para el Home.",
-    details: "Descripción extendida y detalle técnico para el Portfolio.",
-    tech: "React, Node.js, Database",
-    link: "https://github.com/your-username/project-one",
+    title: "Portfolio",
+    image: logoPortfolio,
+    description: "Personal and professional website developed as part of the Master's in React.",
+    details: "Interactive application built with reusable components, focused on efficient state management, optimized performance, and responsive design",
+    tech: "React, Vite, JavaScript, CSS",
+    link: "https://github.com/Rapdev12/Master-en-React/tree/main/08-proyecto-Portfolio",
   },
   {
-    title: "Project Two",
-    image: "/project-two.png",
-    description: "Descripción resumida para el Home.",
-    details: "Descripción extendida y detalle técnico para el Portfolio.",
-    tech: "React, Node.js, Database",
-    link: "https://github.com/your-username/project-one",
+    title: "Pelisweb",
+    image: logoPelisweb,
+    description: "Aplicación web SPA desarrollada en React para gestionar, buscar y organizar un catálogo personal de películas con almacenamiento local.",
+    details: "DDesarrollada como parte de un máster práctico en React, esta aplicación implementa un sistema modular de componentes funcionales y hooks personalizados. Permite añadir nuevas películas con títulos, descripciones e imágenes, realizar búsquedas en tiempo real sobre el listado almacenado, y editar o eliminar registros de forma persistente utilizando el LocalStorage del navegador. Cuenta con un diseño moderno en modo oscuro adaptado con CSS Grid y Flexbox.",
+    tech: "React, Vite, JavaScript (ES6+), LocalStorage, CSS3, HTML5",
+    link: "https://github.com/Rapdev12/Master-en-React/tree/main/06-proyecto-Pelisweb/PelisWeb",
   },
   {
     title: "Project Three",
@@ -23,5 +26,5 @@ export const projects = [
     tech: "React, Node.js, Database",
     link: "https://github.com/your-username/project-one",
   },
-  
+
 ];
